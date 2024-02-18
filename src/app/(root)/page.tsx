7b8a0e27aecfebe1f073e1300c4deba12d6f1248
Carlos Vargas Arce <carlosvargasarce@Carloss-MacBook-Pro.local>
2024-02-18 11:37:13 -0600

    Project Base

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import styles from './page.module.scss';
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Lista de usuarios</h1>
+    </main>
+  );
+}
---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+function Sidebar() {
+  return (
+    <section>
+      <h1>Sidebar</h1>
+    </section>
+  );
+}
+
+export default Sidebar;
