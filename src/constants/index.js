1eed21177adb8d771cc59c1d95671307a7514e0c
Carlos Vargas Arce <carlosvargasarce@Carloss-MacBook-Pro.local>
2024-02-18 22:40:04 -0600

    Title, Bottombar and Sidebar Components

---
+++ src/constants/index.js
@@ -0,0 +1,30 @@
+export const sidebarLinks = [
+  {
+    label: 'Inicio',
+    route: '/',
+    icon: 'Home',
+  },
+  {
+    label: 'Usuarios',
+    route: '/usuarios',
+    icon: 'Every-user',
+  },
+  {
+    label: 'Templates',
+    route: '/templates',
+    icon: 'Page-template',
+  },
+  {
+    label: 'Documentos',
+    route: '/documentos',
+    icon: 'File-focus-one',
+  },
+];
+
+export const colors = {
+  primaryColor: '#00A96C',
+  grayDarkColor: '#525252',
+  grayColor: '#d6d6d6',
+  grayLightColor: '#ededed',
+  whiteColor: '#FFFFFF',
+};
