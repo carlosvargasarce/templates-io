f80ffb084178b48b87609fe940a10ca1c2cf6306
Carlos Vargas Arce <carlosavargasarce@gmail.com>
2024-02-28 00:29:23 -0600

    Crear template completado

--- src/app/(root)/crear-template/page.tsx
+++ src/app/(root)/crear-template/page.tsx
@@ -1,11 +1,17 @@
 'use client';
 
+import Button from '@/components/Button/Button';
 import InputField from '@/components/InputField/InputField';
 import Title from '@/components/Title/Title';
 import useToast from '@/hooks/useToast';
 import TemplateManager from '@/lib/manager/TemplateManager';
+import EditorJs from '@editorjs/editorjs';
+import Embed from '@editorjs/embed';
+import Header from '@editorjs/header';
+import List from '@editorjs/list';
+import SimpleImage from '@editorjs/simple-image';
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import styles from './page.module.scss';
 
 /**
@@ -14,9 +20,39 @@ import styles from './page.module.scss';
  * Permite a los clientes crear un nuevo template proporcionando su nombre, descripción,
  * palabras clave, entre otros.
  */
+
 export default function Page() {
+  const editorRef = useRef<EditorJs | null>(null);
   const router = useRouter();
   const { notifyError } = useToast();
+
+  useEffect(() => {
+    if (typeof window !== 'undefined' && !editorRef.current) {
+      const EditorJS = require('@editorjs/editorjs').default;
+      editorRef.current = new EditorJS({
+        holder: 'editorjs',
+        autofocus: true,
+        placeholder:
+          'Empezemos a crear un asombroso template, usa nombres entre llaves para utilizar como espacios dinámicos, por ejemplo: Mi nombre es {{Nombre Completo}}',
+
+        tools: {
+          simpleImage: SimpleImage,
+          embed: Embed,
+          header: Header,
+          list: List,
+        },
+      });
+
+      return () => {
+        if (
+          editorRef.current &&
+          typeof editorRef.current.destroy === 'function'
+        ) {
+          editorRef.current.destroy();
+        }
+      };
+    }
+  }, []);
 
   // Estado inicial del formulario, incluyendo el manejo de preguntas condicionales.
   const [formData, setFormData] = useState({
@@ -41,36 +77,46 @@ export default function Page() {
     formData.name &&
     formData.description &&
     formData.keywords &&
-    formData.category &&
-    formData.text;
+    formData.category;
 
   /**
    * Maneja el envío del formulario para crear una nueva cuenta.
    * @param {React.FormEvent} e - Evento de envío del formulario.
    */
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    if (isFormValid) {
-      const templateData = {
-        name: formData.name,
-        description: formData.description,
-        // TODO: IMPLEMENTAR UN MEJOR COMPONENTE PARA PALABRAS CLAVE QUE UN INPUT
-        keywords: formData.keywords.split(',').map((keyword) => keyword.trim()),
-        category: formData.category,
-        text: formData.text,
-        id: '',
-      };
 
-      const templateManager = new TemplateManager();
-
-      try {
-        templateManager.createTemplate(templateData);
-        router.push('/templates');
-      } catch (error) {
-        notifyError(`Error al crear template: ${error}`);
-      }
-    } else {
+    if (!isFormValid) {
       notifyError('El formulario no es válido.');
+      return;
+    }
+
+    let templateData = {
+      name: formData.name,
+      description: formData.description,
+      keywords: formData.keywords.split(',').map((keyword) => keyword.trim()),
+      category: formData.category,
+      text: '',
+      id: '',
+    };
+
+    if (editorRef.current) {
+      try {
+        const outputData = await editorRef.current.save();
+        templateData.text = JSON.stringify(outputData);
+      } catch (error) {
+        notifyError(`Error al guardar el contenido del editor: ${error}`);
+        return;
+      }
+    }
+
+    const templateManager = new TemplateManager();
+
+    try {
+      templateManager.createTemplate(templateData);
+      router.push('/templates');
+    } catch (error) {
+      notifyError(`Error al crear template: ${error}`);
     }
   };
 
@@ -120,6 +166,18 @@ export default function Page() {
             value={formData.description}
             onChange={handleChange}
             required
+          />
+        </div>
+        <div className={styles.formControlLong}>
+          <p className={styles.editorLabel}>Texto:</p>
+          <div className={styles.editor} id="editorjs" />
+        </div>
+        <div className={styles.formControl}>
+          <Button
+            label="Crear Template"
+            type="submit"
+            disabled={!isFormValid}
+            style={{ marginBottom: '24px', marginTop: '24px' }}
           />
         </div>
       </form>
---
+++ src/types/editor.d.ts
@@ -0,0 +1,34 @@
+declare module '@editorjs/simple-image' {
+  const SimpleImage: any;
+  export default SimpleImage;
+}
+
+declare module '@editorjs/embed' {
+  const Embed: any;
+  export default Embed;
+}
+
+declare module '@editorjs/link' {
+  const Link: any;
+  export default Link;
+}
+
+declare module '@editorjs/list' {
+  const List: any;
+  export default List;
+}
+
+declare module '@editorjs/quote' {
+  const Quote: any;
+  export default Quote;
+}
+
+declare module '@editorjs/raw' {
+  const Raw: any;
+  export default Raw;
+}
+
+declare module '@editorjs/header' {
+  const Header: any;
+  export default Header;
+}
